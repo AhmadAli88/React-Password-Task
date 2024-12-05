c1728830cae169b0e2734b4afbf12619a96dde64
Ahmad Ali <xevenahmad@gmail.com>
2024-12-05 18:31:11 +0500

    finalize password task

--- src/components/Password.jsx
+++ src/components/Password.jsx
@@ -1,16 +1,50 @@
-import { Col, Row } from 'react-bootstrap';
+import { useState } from 'react';
+
+//components
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
+import { Col, Row } from 'react-bootstrap';
+//icons
+import { FaRegEye } from 'react-icons/fa';
+import { BiSolidHide } from 'react-icons/bi';
 
 const MyPassword = () => {
+  const [showPassword, setShowPassword] = useState(true);
+
+  const handleUpdateState = () => {
+    setShowPassword(!showPassword);
+  };
   return (
     <div className='d-flex justify-content-center align-items-center vh-100 w-100'>
       <Row className='border h-50 w-50 rounded p-3'>
         <Col lg={12} md={6} sm={4}>
           <Form.Group className='h-100 d-flex justify-content-center align-items-center gap-4'>
-            <Form.Label>Password</Form.Label>
+            <div className='position-relative'>
+              <Form.Label>Password</Form.Label>
 
-            <Form.Control type='password' id='password' name='password' />
+              {showPassword ? (
+                <>
+                  <Form.Control type='password' id='password' name='password' />
+                  <BiSolidHide
+                    size={20}
+                    className='position-absolute end-0 top-50'
+                    onClick={handleUpdateState}
+                  />
+                </>
+              ) : (
+                <>
+                  <Form.Control type='text' id='text' name='text' />
+                  <FaRegEye
+                    size={20}
+                    className='position-absolute end-0 top-50'
+                    onClick={handleUpdateState}
+                  />
+                </>
+              )}
+              {/* <FaRegEye size={20} className='position-absolute end-0 top-50' />
+              <BiSolidHide size={20} className='position-absolute end-0 top-50'/> */}
+            </div>
+
             <div className='d-flex justify-content-end align-items-end mt-3 h-100'>
               <Button variant='primary' type='submit'>
                 Submit
